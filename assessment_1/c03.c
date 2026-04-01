558c456e7c77b009e844c04597b3f099f251287b
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-04-01 19:36:46 +0530

    Add files via upload

---
+++ assessment_1/c03.c
@@ -0,0 +1,9 @@
+// 2.Subtrating 5 from a number.
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter Number:");
+    scanf("%d",&x);
+    printf("%d",x-5);
+    return 0;
+}
---
+++ assessment_1/c05.c
@@ -0,0 +1,10 @@
+// 5. divide the number by 8 and print the reminder
+# include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter the number:");
+    scanf("%d",&a);
+    b=a%8;
+    printf("%d",b);
+    return 0;
+}
