d172a55884958813d8bf43e2f94890070cf53e0f
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-04-03 21:24:23 +0530

    Add files via upload

---
+++ c01.c
@@ -0,0 +1,13 @@
+// 1st question
+
+# include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a==50){
+        printf("1");
+    }else{
+        printf("0");
+    }
+    return 0;
+}
---
+++ c02.c
@@ -0,0 +1,13 @@
+// 2nd question
+
+# include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a==50){
+        printf("0");
+    }else{
+        printf("1");
+    }
+    return 0;
+}
