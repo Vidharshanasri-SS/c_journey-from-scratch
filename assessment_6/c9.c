fa526715ffdb78f544ff673208943d537bb2ffdb
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-04-08 18:34:21 +0530

    Add files via upload

---
+++ assessment_6/c9.c
@@ -0,0 +1,13 @@
+// to print the 2-digit odd number whose sum is 7
+# include <stdio.h>
+int main(){
+    int i=11,sum=0;
+    while(i<=99){
+        if(i%10==5){
+            sum=sum+i;
+        }
+        i++;
+    }
+    printf("%d",sum);
+    return 0;
+}
